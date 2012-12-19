20512e7717d247462677b1e60bc6c42b03d59c13
Alpha4 <aure.b21@gmail.com>
2012-12-19 18:26:23 +0100

    Serializer and Deserializer
    Ajout des fonctions serializer et deserializer au Main.java
    Suppression des fichiers de travail Serializer et Deserialis-zer
    
    serializer permet d'enregistrer les listes proprioset reservatiosn dnas
    un fichier
    
    deserializer permet de les importer dans le programme (i.e.: les lire
    au lancement du programme)

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,36 @@ import java.io.*;
 public class Main
 {
 
+	public static void serializer(List proprios,List reservations)
+	{
+		try {
+			FileOutputStream fichier = new FileOutputStream("ossau.ser");
+			ObjectOutputStream oos = new ObjectOutputStream(fichier);
+			oos.writeObject(proprios);
+			oos.writeObject(reservations);
+			oos.flush();
+			oos.close();
+		}
+		catch (java.io.IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void deserializer(List proprios, List reservations) {
+		try
+		{
+			FileInputStream fichier = new FileInputStream("ossau.ser");
+			ObjectInputStream ois = new ObjectInputStream(fichier);
+			proprios= (List) ois.readObject();
+			reservations=(List) ois.readObject();
+		} 
+		catch (java.io.IOException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 	/* Programme principal*/
 	public static void main (String[] args)
 	{
