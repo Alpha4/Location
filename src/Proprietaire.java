d53f3a7852c6d6063860faec73431379ff2d24fd
Kelite <hippo.gandon@live.fr>
2012-12-19 17:51:42 +0100

    Update src/Proprietaire.java

--- src/Proprietaire.java
+++ src/Proprietaire.java
@@ -7,7 +7,7 @@ import java.io.*;
 public abstract class Proprietaire implements java.io.Serializable
 {
 	private String nom, type;
-	private List <Logement> proprietes;
+	private List <Logement> biens;
 	private int ca; //chiffre d'affaire
 	
 	public Proprietaire (String nom,String type,int ca)
@@ -18,4 +18,13 @@ public abstract class Proprietaire implements java.io.Serializable
 		this.proprietes=new Vector <Logement>();
 	}
 	
+	public String toString()
+	{
+		return this.nom + this.ca;
+	}
+	
+	public String Logement()
+	{
+		return this.nom + for (Logement l : biens) { l.toString; }
+	}
 }
