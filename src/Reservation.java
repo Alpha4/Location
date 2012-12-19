19a51c5e8ba37791226cbeca7666a1471e951031
Alpha4 <aure.b21@gmail.com>
2012-12-19 18:39:57 +0100

    Classe Reservation

---
+++ src/Reservation.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+/**	Classe : Reservation
+*	Méthodes :
+*/
+public abstract class Reservation implements java.io.Serializable
+{
+	private Logement logement;
+	private String nomloc;
+	private int adultes,enfants;
+	
+	public Reservation (Logement logement,String nomloc,int adultes,int enfants)
+	{
+		this.logement=logement;
+		this.nomloc=nomloc;
+		this.adultes=adultes;
+		this.enfants=enfants;		
+	}
