ba7a4832df1435a7b305eb00b44225d48ace7008
Alpha4 <aure.b21@gmail.com>
2012-12-19 16:12:42 +0100

    Calsse Entreprise

---
+++ src/Entreprise.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Random;
+
+/**	Classe : Entreprise
+*	Méthodes :
+*/
+public class Entreprise extends Proprietaire
+{
+		
+	public Entreprise (String nom,String type,int ca)
+	{
+		super(nom,type,ca);
+	}
+	
+}
