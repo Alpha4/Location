2862d69bde799ddf3869046b4af4aecfc1e80f7d
Alpha4 <aure.b21@gmail.com>
2012-12-19 16:05:40 +0100

    Classe Particulier

---
+++ src/Particulier.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Random;
+
+/**	Classe : Particulier
+*	Méthodes :
+*/
+public class Particulier extends Proprietaire
+{
+		
+	public Particulier (String nom,String type,int ca)
+	{
+		super(nom,type,ca);
+	}
+	
+}
