7e49528505ae8a5d6e667b312abc09480bca0034
Kelite <hippo.gandon@live.fr>
2012-12-20 13:53:07 +0100

    Update src/Maison.java

--- src/Maison.java
+++ src/Maison.java
@@ -5,7 +5,13 @@ import java.io.*;
 *	Méthodes :
 */
 public class Maison extends Logement
-{
+{	
+	
+	/** Constructeur
+	*	avec saisie de l'adresse et du prix
+	*	et automatisation du type et de la capacité
+	* @see Logement#Logement(String type, int capacite, String adresse, int prix)
+	*/
 	public Maison (String adresse,int prixfixe)
 	{
 		super("Maison",10,adresse,prixfixe);	
