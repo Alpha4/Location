fb78c53dc600c5475c192525f16f8445299ce6ef
Kelite <hippo.gandon@live.fr>
2012-12-20 15:09:09 +0100

    Update src/Entreprise.java

--- src/Entreprise.java
+++ src/Entreprise.java
@@ -3,10 +3,14 @@ import java.io.*;
 
 /**	Classe : Entreprise
 *	Méthodes :
+* 		Constructeur avec saisie des données
 */
 public class Entreprise extends Proprietaire
 {
-		
+	
+	/** Constructeur
+	*	avec saisie des données
+	*/
 	public Entreprise (String nom,String type,int ca)
 	{
 		super(nom,type,ca);
