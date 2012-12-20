580438e02799d49888f07d8973e27fa0b1d3eae3
Kelite <hippo.gandon@live.fr>
2012-12-20 15:20:24 +0100

    Update src/Appartement.java

--- src/Appartement.java
+++ src/Appartement.java
@@ -4,7 +4,7 @@ import java.util.Random;
 /**  Classe : Appartement
 *	Méthodes :
 * 		Constructeur avec saisie et données automatique
-* 		Méthode de calcul du prix
+* 		Méthode de calcul du prix en fonction du nombre d'adultes et d'enfants
 */
 public class Appartement extends Logement
 {	
