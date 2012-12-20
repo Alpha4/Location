b2272c01df4bb7f67d3bd16ad82a4ce284f54b9d
Kelite <hippo.gandon@live.fr>
2012-12-20 15:26:54 +0100

    Update src/Reservation.java

--- src/Reservation.java
+++ src/Reservation.java
@@ -3,6 +3,7 @@ import java.io.*;
 
 /**	Classe : Reservation
 *	Méthodes :
+* 		Constructeur avec saisie des données
 */
 public abstract class Reservation implements java.io.Serializable
 {
@@ -10,6 +11,9 @@ public abstract class Reservation implements java.io.Serializable
 	private String nomloc;
 	private int adultes,enfants;
 	
+	/** Constructeur
+	*	avec saisie des données
+	*/
 	public Reservation (Logement logement,String nomloc,int adultes,int enfants)
 	{
 		this.logement=logement;
