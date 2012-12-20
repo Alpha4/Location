db2320ca208c989a576107e518f3cfee651730c6
Kelite <hippo.gandon@live.fr>
2012-12-20 15:59:55 +0100

    Update src/Reservation.java

--- src/Reservation.java
+++ src/Reservation.java
@@ -4,6 +4,7 @@ import java.io.*;
 /**	Classe : Reservation
 *	Méthodes :
 * 		Constructeur avec saisie des données
+* 		Méthode retournant le nom du locataire principal et le prix
 */
 public abstract class Reservation implements java.io.Serializable
 {
@@ -20,5 +21,16 @@ public abstract class Reservation implements java.io.Serializable
 		this.nomloc=nomloc;
 		this.adultes=adultes;
 		this.enfants=enfants;		
+	}
+	
+	/** Méthode toString retournant le nom du
+	*	locataire principal et le prix
+	* @return nomloc le nom du locataire principal
+	* @return prix le prix du logement
+	* @see Logement#getPrix
+	*/
+	public toString()
+	{
+		return this.nomloc+
 	}
 }
