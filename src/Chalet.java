03c615cf4ea218a4a841fc355a2b331b9a78a0ec
Kelite <hippo.gandon@live.fr>
2012-12-20 15:29:41 +0100

    Update src/Chalet.java

--- src/Chalet.java
+++ src/Chalet.java
@@ -4,7 +4,7 @@ import java.io.*;
 /**	Classe : Chalet
 *	Méthodes :
 * 		Constructeur avec saisie et données automatique
-* 		Méthode de calcul du prix en fonction du nombre d'adultes et d'enfants
+* 		Méthode de calcul du prix en fonction du nombre d'adultes (nba) et d'enfants (nbe)
 */
 public class Chalet extends Logement
 {	
