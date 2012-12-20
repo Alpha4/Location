8ded410064ecd79af9a7da230e2a4f0d039cefbc
Kelite <hippo.gandon@live.fr>
2012-12-20 14:07:20 +0100

    Update src/Proprietaire.java

--- src/Proprietaire.java
+++ src/Proprietaire.java
@@ -10,6 +10,9 @@ public abstract class Proprietaire implements java.io.Serializable
 	private List <Logement> biens;
 	private int ca; //chiffre d'affaire
 	
+	/** Constructeur
+	*	avec saisie des données
+	*/
 	public Proprietaire (String nom,String type,int ca)
 	{
 		this.nom=nom;
@@ -18,21 +21,37 @@ public abstract class Proprietaire implements java.io.Serializable
 		this.biens=new Vector <Logement>();
 	}
 	
+	/** getNom : retourne le nom du propriétaire
+	* @return nom le nom du propriétaire
+	*/
 	public String getNom()
 	{
 		return this.nom;
 	}
 	
+	/** getCa : retourne le chiffre d'affaire du propriétaire
+	* @return ca le chiffre d'affaire du propriétaire
+	*/
 	public int getCa()
 	{
 		return this.ca;
 	}
 	
+	/** Méthode toString retournant le nom et
+	*	le chiffre d'affaire du prorpiétaire
+	* @return nom le nom du propriétaire
+	* @return ca le chiffre d'affaire du propriétaire
+	*/
 	public String toString()
 	{
 		return this.nom + this.ca;
 	}
 	
+	/** Méthode Logement retournant la liste des
+	*	logements du prorpiétaire
+	* @return logement la liste des logements
+	* @see Logement#toString()
+	*/
 	public String Logement()
 	{
 		String logement="";
