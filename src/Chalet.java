6a662bc143e1e15e0d33bc136141ef952941c7aa
Kelite <hippo.gandon@live.fr>
2012-12-20 13:52:42 +0100

    Update src/Chalet.java

--- src/Chalet.java
+++ src/Chalet.java
@@ -6,6 +6,12 @@ import java.io.*;
 */
 public class Chalet extends Logement
 {	
+	
+	/** Constructeur
+	*	avec saisie de l'adresse et du prix
+	*	et automatisation du type et de la capacité
+	* @see Logement#Logement(String type, int capacite, String adresse, int prix)
+	*/
 	public Chalet (String adresse,int prix)
 	{
 		super("Chalet",6,adresse,prix);	
