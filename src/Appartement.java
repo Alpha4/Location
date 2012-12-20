c9c8e57115a049d74da33a44682ceaf4382825b1
Kelite <hippo.gandon@live.fr>
2012-12-20 13:50:15 +0100

    Update src/Appartement.java

--- src/Appartement.java
+++ src/Appartement.java
@@ -6,10 +6,22 @@ import java.util.Random;
 */
 public class Appartement extends Logement
 {	
+	
+	/** Constructeur
+	*	avec saisie de l'adresse et du prix
+	*	et automatisation du type et de la capacité
+	* @see Logement#Logement
+	*/
 	public Appartement (String adresse,int prix)
 	{
 		super("Appartement",3,adresse,prix);	
 	}
+	
+	/** Méthode permettant de calculer le prix en fonction
+	*	du nombre d'enfants et d'adultes
+	* @return prix le prix
+	* @see getPrix#Logement
+	*/
 	public int calculprix(int nba,int nbe)
 	{
 		return getPrix();
