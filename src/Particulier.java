8ff4a7240aef4acb47259ddc2e2bdc18172949e7
Kelite <hippo.gandon@live.fr>
2012-12-20 15:07:53 +0100

    Update src/Particulier.java

--- src/Particulier.java
+++ src/Particulier.java
@@ -3,10 +3,14 @@ import java.io.*;
 
 /**	Classe : Particulier
 *	Méthodes :
+* 		Constructeur avec saisie des données
 */
 public class Particulier extends Proprietaire
 {
-		
+	
+	/** Constructeur
+	*	avec saisie des données
+	*/
 	public Particulier (String nom,String type,int ca)
 	{
 		super(nom,type,ca);
